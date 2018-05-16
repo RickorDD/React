f6f9923973c39e0bb157ee8caf64aa365e228ba7
Reiko <reikoweiss@Reikos-MBP.fritz.box>
2018-05-16 08:17:58 +0200

    2. Commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
-import Button from "./Button";
-import Panel from "./Panel";
 import './App.css';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      name: "Welt"
-    }
-
-    setTimeout(() => {
-      // this.state.name = "Venus";
-      this.setState({
-        name: "Venus"
-      })
-    }, 3000);
   }
 
   render() {
     return (
       <div className="App">
-        <h2>Hallo {this.state.name}!</h2>
-        <p>{this.state.name.length}</p>
+        <h1>Todo-Liste</h1>
       </div>
     );
   }
